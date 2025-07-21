3e53e60bc9abac61671ed92335fe6d506650598a
Claudio Paredes <claudioparedesarbelo@gmail.com>
2025-07-21 20:51:09 -0300

    function high order

--- js/highorder.js
+++ js/highorder.js
@@ -86,23 +86,107 @@ porCadaUno(numeros, (el)=>{duplicado.push(el*2)})
 
 console.log(duplicado); */
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ /* const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-numeros.forEach((numero)=>{
+ const numerosTotal = numeros.reduce((acumulador, numero)=> acumulador + numero, 0);
+
+ console.log(numerosTotal); */
+
+/* numeros.forEach((numero)=>{
     console.log(numero**2);
-})
+}) */
 
-const cursos = [
+/* const cursos = [
     {nombre: "Curso 1", horas: 10},
     {nombre: "Curso 2", horas: 20},
     {nombre: "Curso 2", horas: 30},
     {nombre: "Curso 3", horas: 30},
-]
+] */
 
-let resultado = cursos.find((i)=> i.nombre === "Curso 2");
+/* let resultado = cursos.find((i)=> i.nombre === "Curso 2");
 
 console.log(resultado);
 
 let resultadoFilter = cursos.filter((i)=> i.horas < 30);
 
 console.log(resultadoFilter);
+
+console.log(cursos.some((i)=>i.nombre === "Curso 2"));
+
+ */
+/* const resultadoMap = cursos.map((i)=> {
+    return {
+        Nombre: i.nombre + ' ' + 'Pasado por el map',
+        Horas: i.horas + 10
+    }
+});
+
+console.log(resultadoMap); */
+
+/* const products = [
+    {name: "Product 1", price: 10},
+    {name: "Product 2", price: 20},
+    {name: "Product 3", price: 30},
+    {name: "Product 4", price: 40},
+    {name: "Product 5", price: 50},
+]
+
+const resultadoMap = products.map((i)=> {
+    return {
+        Nombre: i.name,
+        Precio: i.price * 1.21
+    }
+});
+
+console.log(resultadoMap);
+ */
+/* 
+const numerosParaOrdenar = [ 3, 12, 20, 1, 5, 24, 14, 16]
+
+console.log(Math.max(...numerosParaOrdenar));
+
+console.log(Math.ceil(2.56))
+
+console.log(Math.floor(2.56))
+
+console.log(Math.round(2.46))
+ */
+
+
+
+
+/* numerosParaOrdenar.sort((a, b) => a - b);
+
+console.log(numerosParaOrdenar);
+
+numerosParaOrdenar.sort((a, b)=> b - a)
+
+console.log(numerosParaOrdenar);
+
+const nombres = ["Alexis", "Ana", "Jony", "Hipolito", "Martin"]
+
+nombres.sort((a, b) => b - a);
+
+console.log(nombres); */
+
+/* console.log(new Date()); */
+
+const hoy = new Date('July 20, 2025');
+
+console.log(hoy.getFullYear());
+
+console.log(hoy.getMonth());
+
+console.log(hoy.getDate());
+
+console.log(hoy.toDateString());
+
+console.log(hoy.toLocaleString());
+
+console.log(hoy.toLocaleDateString());
+
+console.log(hoy.toLocaleTimeString());
+
+console.log(hoy.toTimeString());
+
+console.log(hoy.getDay())
