c7a29d0c7fd4f473a07a99867dc1c4a2d65be7e1
Claudio Paredes <claudioparedesarbelo@gmail.com>
2025-07-23 12:43:03 -0300

    dom

---
+++ js/dom.js
@@ -0,0 +1,35 @@
+/* let contenedor = document.getElementById('container');
+
+contenedor.innerHTML = '<h1>Hola Mundo</h1>';
+
+let titulo = document.getElementById('titulo');
+
+console.log(titulo.innerHTML);
+
+let paises = document.getElementsByClassName('paises');
+
+console.log(paises[7].innerHTML);
+
+let etiqueta = document.getElementsByTagName('p');
+
+console.log(etiqueta[0].innerHTML)
+
+console.log("------------------------------")
+
+for (const pais of paises) {
+    console.log(pais.innerHTML);  
+}
+
+let capturar = document.getElementById("insertar");
+
+console.log(capturar.innerHTML);
+
+capturar.innerHTML = "Hola Curso FrontEnd 2025";
+
+console.log(capturar.innerHTML);
+
+contenedor.innerHTML = '<h1>Hola Mundo 2025</h1> <p>Esto es desde js</p> <h3>Aca es una etiqueta h3</h3>'; */
+
+let contenedor = document.getElementById('container');
+
+contenedor.className = 'nuevoNombre'
